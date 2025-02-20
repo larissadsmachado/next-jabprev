09cef3a88673d8bcd6a12ec59e4c635f8805df35
Larissa Machado <larissadsmachado0@gmail.com>
2025-02-20 16:59:00 -0300

    enquete

--- src/app/components/Enquete/index.tsx
+++ src/app/components/Enquete/index.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import { CheckCircle, Loader2, XCircle, ThumbsDown, Meh, ThumbsUp, Star } from "lucide-react";
+import {
+  CheckCircle,
+  Loader2,
+  XCircle,
+  ThumbsDown,
+  Meh,
+  ThumbsUp,
+  Star,
+} from "lucide-react";
+
+
 
 const FeedbackForm = () => {
   const [nota, setNota] = useState("");
@@ -48,36 +58,56 @@ const FeedbackForm = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto bg-white shadow-lg rounded-2xl p-6 border">
-      <h2 className="text-2xl font-bold text-center mb-4 text-gray-800">Avalie nosso serviço</h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="grid grid-cols-2 gap-2">
-          {notas.map(({ label, icon }) => (
+    <div className="flex justify-center items-center bg-gradient-to-b from-[#003470] via-[#9ccef0] to-[#003470] py-36">
+      <div className="">
+        <h2 className="text-4xl font-bold text-center mb-4 text-white uppercase pb-3">
+          Avalie nosso serviço
+        </h2>
+
+        <div className="max-w-md mx-auto bg-white border-2 border-gray-300 shadow-lg rounded-2xl p-6 ">
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="grid grid-cols-2 gap-2">
+              {notas.map(({ label, icon }) => (
+                <button
+                  key={label}
+                  type="button"
+                  onClick={() => setNota(label)}
+                  className={`flex items-center justify-center gap-2 p-3 rounded-lg border w-full text-center transition-all ${
+                    nota === label
+                      ? "bg-blue-800 text-white border-2 border-blue-300"
+                      : "bg-gray-100 text-gray-800 hover:bg-gray-200 border-2 border-gray-400"
+                  }`}
+                >
+                  {icon} {label}
+                </button>
+              ))}
+            </div>
             <button
-              key={label}
-              type="button"
-              onClick={() => setNota(label)}
-              className={`flex items-center justify-center gap-2 p-3 rounded-lg border w-full text-center transition-all ${
-                nota === label ? "bg-blue-500 text-white border-blue-500" : "bg-gray-100 text-gray-800 hover:bg-gray-200"
-              }`}
+              type="submit"
+              className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-800 transition-all flex items-center justify-center"
+              disabled={loading}
             >
-              {icon} {label}
+              {loading ? (
+                <Loader2 className="animate-spin mr-2" size={20} />
+              ) : (
+                "Enviar Avaliação"
+              )}
             </button>
-          ))}
+          </form>
+          {message && (
+            <p className="mt-4 text-center text-lg font-semibold flex items-center justify-center gap-2">
+              {message.includes("sucesso") ? (
+                <CheckCircle className="text-green-500" size={20} />
+              ) : (
+                "❌"
+              )}{" "}
+              {message}
+            </p>
+          )}
         </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition-all flex items-center justify-center"
-          disabled={loading}
-        >
-          {loading ? <Loader2 className="animate-spin mr-2" size={20} /> : "Enviar Avaliação"}
-        </button>
-      </form>
-      {message && (
-        <p className="mt-4 text-center text-lg font-semibold flex items-center justify-center gap-2">
-          {message.includes("sucesso") ? <CheckCircle className="text-green-500" size={20} /> : "❌"} {message}
-        </p>
-      )}
+
+
+      </div>
     </div>
   );
 };
