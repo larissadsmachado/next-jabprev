eec89a97bbe7b341478327603412d0a2e3752998
Larissa Machado <larissadsmachado0@gmail.com>
2025-02-24 16:59:08 -0300

    terminar despesas

--- src/app/Despesas-Administrativas/conteudo.tsx
+++ src/app/Despesas-Administrativas/conteudo.tsx
@@ -1,77 +1,207 @@
 const data = {
-
   2025: {
-    Janeiro: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2025/02/GERAL.pdf",
-    "R$ 293.702,83": "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2025/02/GERAL.pdf",
-
-    Fevereiro: "#",
-    "Fevereiro - Câmara": "#",
-
-    Março: "#",
-    "Março - Câmara":"#",
-
-    Abril:"#",
-    "Abril – Câmara":"#",
-
-    Maio:"#",
-    "Maio – Câmara":"#",
-
-    Junho:"#",
-    "Junho – Câmara ":"#",
-
-    Julho:"#",
-    "Julho – Câmara ":"#",
-
-    Agosto:"#",
-    "Agosto – Câmara":"#",
-
-    Setembro:"#",
-    Outubro:"#",
-    "Outubro – Câmara ":"#",
-
-    Novembro:"#",
-    "Novembro – Câmara":"#",
-
-    Dezembro:"#",
-    "Dezembro - Câmara":"#",
-
-    "13º Salário":"#",
-    "13º Salário – Câmara":"#",
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { 
+      valor: "R$ 293.702,83", 
+      link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2025/02/GERAL.pdf" 
+    },
   },
 
+  2024: {
+    "Dezembro": { valor: "R$ 273.397,80", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2025/02/12-DEZEMBRO-DESPESAS-ADM-2024.pdf" },
+    "Novembro": { valor: "R$ 199.986,64", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/12/11-NOVEMBRO-DESPESAS-ADM-2024.pdf" },
+    "Outubro": { valor: "R$ 188.497,11", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/11/10-OUTUBRO-DESPESAS-ADM-2024.pdf" },
+    "Setembro": { valor: "R$ 178.424,74", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/10/09-SETEMBRO-DESPESAS-ADM-2024.pdf" },
+    "Agosto": { valor: "R$ 182.766,35", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/09/08-AGOSTO-DESPESAS-ADM-2024.pdf" },
+    "Julho": { valor: "R$ 174.593,45", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/08/07-JULHO-DESPESAS-ADM-2024.pdf" },
+    "Junho": { valor: "R$ 251.071,67", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/07/06-JUNHO-DESPESAS-ADM-2024.pdf" },
+    "Maio": { valor: "R$ 188.216,44", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/07/05-MAIO-DESPESAS-ADM-2024.pdf" },
+    "Abril": { valor: "R$ 198.584,82", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/07/04-ABRIL-DESPESAS-ADM-2024.pdf" },
+    "Março": { valor: "R$ 169.437,17", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/07/03-MARCO-DESPESAS-ADM-2024.pdf" },
+    "Fevereiro": { valor: "R$ 198.305,44", link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/07/02-FEVEREIRO-DESPESAS-ADM-2024.pdf" },
+    "Janeiro": { 
+      valor: "R$ 148.980,29", 
+      link: "https://jaboataoprev.jaboatao.pe.gov.br/wp-content/uploads/2024/07/01-JANEIRO-DESPESAS-ADM-2024.pdf" 
+    },
+  },
+ 
+  2023: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
 
+  2022: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
 
+  2021: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
+
+  2020: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
+
+  2019: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
+
+  2018: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
+
+  2017: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
+
+  2016: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  },
+
+  2015: {
+    "Dezembro": { valor: "R$", link: "#" },
+    "Novembro": { valor: "R$", link: "#" },
+    "Outubro": { valor: "R$", link: "#" },
+    "Setembro": { valor: "R$", link: "#" },
+    "Agosto": { valor: "R$", link: "#" },
+    "Julho": { valor: "R$", link: "#" },
+    "Junho": { valor: "R$", link: "#" },
+    "Maio": { valor: "R$", link: "#" },
+    "Abril": { valor: "R$", link: "#" },
+    "Março": { valor: "R$", link: "#" },
+    "Fevereiro": { valor: "R$", link: "#" },
+    "Janeiro": { valor: "R$", link: "#" },
+  }
 };
 
 const Links = () => {
   return (
     <div className="h-auto bg-gradient-to-b from-[#cee5ff] to-[#fff4be] p-10">
-  {Object.entries(data)
-    .sort(([yearA], [yearB]) => Number(yearB) - Number(yearA)) // Ordena do maior para o menor
-    .map(([year, links]) => (
-      <div key={year} className="mb-10 max-w-6xl mx-auto">
-        {/* 🔹 Título do ano */}
-        <h1 className="text-4xl font-bold text-blue-900 mb-4 py-4">{year}</h1>
-        
-        {/* 🔹 Lista contínua de links */}
-        <div className="grid grid-cols-2 gap-4">
-          {Object.entries(links).map(([title, link]) => (
-            <a
-              key={title}
-              href={link}
-              target="_blank"
-              className={`text-xl hover:underline ${
-                link === "#" ? "text-gray-500 cursor-default pointer-events-none" : "text-[#051f60]"
-              }`}
-            >
-              {title}
-            </a>
-          ))}
-        </div>
-      </div>
-    ))}
-</div>
+      {Object.entries(data)
+        .sort(([yearA], [yearB]) => Number(yearB) - Number(yearA))
+        .map(([year, months]) => (
+          <div key={year} className="mb-10 max-w-5xl mx-auto">
+            {/* 🔹 Título do ano */}
+            <h1 className="Montserrat-Bold text-4xl font-bold text-blue-900 mb-4 py-4 uppercase">
+              Despesas de {year}
+            </h1>
 
+            {/* 🔹 Lista formatada com valores ao lado */}
+            <div className="flex flex-col gap-2">
+              {Object.entries(months).map(([month, { valor, link }]) => (
+                <a
+                  key={month}
+                  href={link}
+                  target="_blank"
+                  className={`text-xl flex justify-between hover:underline ${
+                    link === "#" ? "text-gray-500 cursor-default pointer-events-none" : "text-[#051f60]"
+                  }`}
+                >
+                  <span>{month}</span>
+                  <span>{valor}</span>
+                </a>
+              ))}
+            </div>
+          </div>
+        ))}
+    </div>
   );
 };
 
