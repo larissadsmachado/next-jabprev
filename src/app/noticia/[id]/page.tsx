a6cb456b2c61c794561959b440cecc41269f3fc7
millemara <barbosaj901@gmail.com>
2025-03-14 11:17:07 -0300

    not

--- src/app/noticia/[id]/page.tsx
+++ src/app/noticia/[id]/page.tsx
@@ -21,7 +21,6 @@ interface Post {
 }
 
 const NoticiaDetalhada = ({ post }: { post: Post }) => {
-
   const formattedDate = new Date(post.date).toLocaleDateString("pt-BR");
   const formattedTime = new Date(post.date).toLocaleTimeString("pt-BR", {
     hour: "2-digit",
@@ -44,54 +43,46 @@ const NoticiaDetalhada = ({ post }: { post: Post }) => {
         <div className="absolute inset-0 bg-black bg-opacity-20 flex flex-col justify-center items-center text-white p-6 ">
 
           <div className="absolute bottom-0 left-0 w-full bg-[#00347070] p-[32px] drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]">
+            <h1 className="Montserrat-Bold uppercase text-[40px] text-white text-center">{post.title.rendered}</h1>
 
-          <h1 className="Montserrat-Bold uppercase text-[40px] text-white text-center">{post.title.rendered}</h1>
-
-          <div className="flex gap-4 mt-2 items-center justify-center">
-            <div className="flex items-center gap-2">
-              <FaCalendarAlt />
-              <span>{formattedDate}</span>
+            <div className="flex gap-4 mt-2 items-center justify-center">
+              <div className="flex items-center gap-2">
+                <FaCalendarAlt />
+                <span>{formattedDate}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <FaClock />
+                <span>{formattedTime}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <FaTag />
+                <span>{post.category || "Sem categoria"}</span>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <FaClock />
-              <span>{formattedTime}</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <FaTag />
-              <span>{post.category || "Sem categoria"}</span>
-            </div>
-          </div>
-
-          
           </div>
         </div>
       </div>
-
 
       {/* Conteúdo da notícia */}
       <section className="my-20 max-w-6xl mx-auto shadow-2xl bg-white p-10">
         <h1 className="Montserrat-Bold text-5xl font-bold mb-4 text-center pb-5">{post.title.rendered}</h1>
 
-
         <div className="flex gap-4 mt-2 mb-5 items-center justify-center">
-            <div className="flex items-center gap-2">
-              <FaCalendarAlt />
-              <span>{formattedDate}</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <FaClock />
-              <span>{formattedTime}</span>
-            </div>
+          <div className="flex items-center gap-2">
+            <FaCalendarAlt />
+            <span>{formattedDate}</span>
           </div>
-
+          <div className="flex items-center gap-2">
+            <FaClock />
+            <span>{formattedTime}</span>
+          </div>
+        </div>
 
         <div className="montserrat-regular prose prose-lg max-w-none text-xl" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
       </section>
     </main>
   );
 };
-
-
 
 export default async function NoticiaPage({ params }: NoticiaPageProps) {
   if (!params?.id) {
