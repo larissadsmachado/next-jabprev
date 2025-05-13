e0488c1d51a2893f9a67f8a6b4ed8cb0e1f0b7db
Larissa Machado <larissadsmachado0@gmail.com>
2025-05-13 14:26:33 -0300

    crp

--- src/app/crp-certidao-de-regularidade-previdenciaria/conteudo.tsx
+++ src/app/crp-certidao-de-regularidade-previdenciaria/conteudo.tsx
@@ -1,36 +1,49 @@
 import React from "react";
 
-
 interface ReportsByYear {
   [year: string]: {
     title: string;
-    url: string;
+    certificates: {
+      name: string;
+      url: string;
+    }[];
   };
 }
 
 const reportsData: ReportsByYear = {
-  "2025": { title: "2025", url: "Pdf/crp/CRP-2025_30.07.25.pdf" },
+  "2025": {
+    title: "2025",
+    certificates: [
+      {
+        name: "CRP – Certificado de Regularidade Previdenciária",
+        url: "Pdf/cad-prev/crp/CRP-2025_30.07.25.pdf",
+      },
+    ],
+  },
 };
+
 const FinancialReports: React.FC = () => {
   return (
     <div className="bg-gradient-to-b from-[#ffffff] via-[#dcecff] to-[#cbe1fc] pb-20 flex flex-wrap justify-center gap-10 py-20">
-
       {Object.keys(reportsData)
         .sort((a, b) => Number(b) - Number(a))
         .map((year) => {
-          const { title, url } = reportsData[year];
+          const { title, certificates } = reportsData[year];
           return (
             <div key={year} className="text-center">
               <h2 className="text-5xl font-bold text-blue-900 Montserrat-Bold">{title}</h2>
               <hr className="border-t-2 border-blue-900 w-20 my-2 mx-auto" />
-              <a
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-800 hover:underline text-2xl Montserrat-Medium"
-              >
-                CRP – Certificado de Regularidade Previdenciária 
-              </a>
+              {certificates.map((certificate, index) => (
+                <a
+                  key={index}
+                  href={certificate.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-800 hover:underline text-2xl Montserrat-Medium block mb-2"
+                >
+                  {certificate.name}
+                </a>
+              ))}
             </div>
           );
         })}
