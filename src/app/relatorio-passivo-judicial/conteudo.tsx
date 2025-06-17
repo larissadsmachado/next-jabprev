4d1dcd4b653ce032ae98d1168d1d7991e7318db0
Larissa Machado <larissadsmachado0@gmail.com>
2025-06-17 16:01:53 -0300

    criação de Relatório do passivo judicial

--- src/app/relatorio-passivo-judicial/conteudo.tsx
+++ src/app/relatorio-passivo-judicial/conteudo.tsx
@@ -53,34 +53,85 @@ const processos: ProcessoData[] = [
     juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
     assunto: "MANDADO DE SEGURANÇA",
   },
+  {
+    processo: "0061728-03.2023.8.17.2810",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "PENSÃO POR MORTE",
+  },
+  {
+    processo: "0031046-31.2024.8.17.2810",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "LICENÇA PRÊMIO",
+  },
+  {
+    processo: "0000590-64.2025.8.17.2810",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "LICENÇA PRÊMIO",
+  },
+  {
+    processo: "0000699-78.2025.8.17.2810",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "LICENÇA PRÊMIO",
+  },
+  {
+    processo: "0027004-36.2024.8.17.2810",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "ISENÇÃO - IR",
+  },
+  {
+    processo: "0029403-09.2022.8.17.2810",
+    juizo: "1ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "PENSÃO POR MORTE",
+  },
+  {
+    processo: "0039463-46.2019.8.17.2810",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "LIQUIDAÇÃO DE SENTENÇA",
+  },
+  {
+    processo: "0126989-77.2024.8.17.2001",
+    juizo: "2ª VARA DA FAZENDA PÚBLICA DA COMARCA DE JABOATÃO DOS GUARARAPES",
+    assunto: "DEFESA - DECLARAÇÃO DE ISENÇÃO DE IR C/C REPETIÇÃO DE INDÉBITO",
+  },
 ];
 
 const JudicialReportTable: React.FC = () => {
   return (
-    <div className="p-6">
-      <h2 className="text-xl font-bold text-center mb-6 text-[#003B73]">
-        RELATÓRIO DO PASSIVO JUDICIAL DO JABOATÃOPREV
-      </h2>
+    <div className="py-14 max-w-[1500px] mx-auto">
 
-      <div className="overflow-x-auto max-w-7xl mx-auto">
-        <table className="min-w-full border border-gray-400">
-          <thead className="bg-[#3498db] text-white text-sm uppercase">
-            <tr>
-              <th className="border border-gray-400 px-4 py-2">Processo</th>
-              <th className="border border-gray-400 px-4 py-2">Juízo</th>
-              <th className="border border-gray-400 px-4 py-2">Assunto</th>
-            </tr>
-          </thead>
-          <tbody className="text-sm text-gray-800">
-            {processos.map((item, index) => (
-              <tr key={index} className="even:bg-gray-100">
-                <td className="border border-gray-400 px-4 py-2">{item.processo}</td>
-                <td className="border border-gray-400 px-4 py-2">{item.juizo}</td>
-                <td className="border border-gray-400 px-4 py-2">{item.assunto}</td>
+      <div className="w-full overflow-x-auto">
+        <div className="min-w-[768px]">
+          <table className="w-full border border-gray-300">
+            <thead className="bg-[#3498db] text-white text-base md:text-lg uppercase">
+              <tr>
+                <th className="border border-gray-300 px-3 py-2 text-left">
+                  Processo
+                </th>
+                <th className="border border-gray-300 px-3 py-2 text-left">
+                  Juízo
+                </th>
+                <th className="border border-gray-300 px-3 py-2 text-left">
+                  Assunto
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="text-sm md:text-base text-gray-800">
+              {processos.map((item, index) => (
+                <tr key={index} className="even:bg-gray-100">
+                  <td className="border border-gray-300 px-3 py-2">
+                    {item.processo}
+                  </td>
+                  <td className="border border-gray-300 px-3 py-2">
+                    {item.juizo}
+                  </td>
+                  <td className="border border-gray-300 px-3 py-2">
+                    {item.assunto}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
