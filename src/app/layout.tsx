c67604437ffd1f8e013f3685a0efd82b1e8d4f01
Larissa Machado <larissadsmachado0@gmail.com>
2025-09-08 10:52:38 -0300

    Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import ScrollToTop from './components/ScrollToTop/index';
 import Footer from './components/Footer/index';
 import Vlibras from './components/Vlibras/index'; 
 import AccessibilityButton from "./components/AccessibilityButton/index";
-
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -14,10 +14,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br"
-    suppressHydrationWarning>
-      <body 
-        cz-shortcut-listen="true">
+    <html lang="pt-br" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5XX7RLTEGY"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5XX7RLTEGY');
+          `}
+        </Script>
+      </head>
+      <body cz-shortcut-listen="true">
         <Navbar />
         {children}
         <AccessibilityButton />
