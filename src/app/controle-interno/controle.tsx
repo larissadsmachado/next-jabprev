08ecce959b859b27ab6eebebf0d353e58ab9adaa
Larissa Machado <larissadsmachado0@gmail.com>
2025-11-28 09:35:43 -0300

    controle interrno

--- src/app/controle-interno/controle.tsx
+++ src/app/controle-interno/controle.tsx
@@ -39,6 +39,10 @@ const Links = () => (
           href: "Pdf/institucional/controle-interno/1.1_Relatorio_Trimestral_de_Controle_Interno___2__TRIMESTRE___2025___JABOATAOPREV.pdf",
           text: "Relatório de Controle Interno – 2º Trimestre",
         },
+        {
+          href: "Pdf/institucional/controle-interno/1.1_Relatorio_Trimestral_de_Controle_Interno___3__TRIMESTRE___2025___JABOATAOPREV.pdf",
+          text: "Relatório de Controle Interno – 3º Trimestre",
+        },
       
       ]} />
 
